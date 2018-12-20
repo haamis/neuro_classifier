4632dd0b67391622545fa1ab4fb99ee630de3935
Antti Virtanen <haama92@gmail.com>
2018-12-20 18:53:52 +0200

    Processing input data.

--- classifier_test.py
+++ classifier_test.py
@@ -11,6 +11,8 @@ from keras.datasets import imdb
 
 import sys
 import json
+import re
+from tqdm import tqdm
 
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
@@ -25,14 +27,35 @@ filters = 250
 kernel_size = 3
 epochs = 2
 
+print("Reading input file..")
+
 with open("./" + sys.argv[1]) as f:
     data = json.load(f)
-print(data[0])
 
-sys.exit()
+abstracts = []
+is_neuro = []
+
+# Neuroscience MeSH-terms.
+reg = re.compile(r"(A08*|A11.650*|A08.637*|A11.671*|A08.675*|C10*|F01*|F02*|F03*|F04*)")
+
+for article in tqdm(data, desc="Grabbing abstracts and mesh terms"):
+    abstract_parts = []
+    abstracts.append("\n".join([x["text"] for x in article["abstract"]]))
+    is_neuro.append(0)
+    for mesh_term in article["mesh_list"]:
+        if reg.match(mesh_term["mesh_id"]):
+            is_neuro[-1] = 1
+            break
+
+del data # Release the huge chunk of memory, ugly. TODO: structure code properly.
+
+print(abstracts[0])
+print(is_neuro[0])
+
+#input("check ram")
 
 print('Loading data...')
-#(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
 
