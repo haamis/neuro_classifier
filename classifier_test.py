cc7bbc97b6e10c87ecafeb9e251f523791bb6e14
Antti Virtanen <haama92@gmail.com>
2018-12-21 17:40:23 +0200

    Vectorizing and transforming data, saving those to disk.

--- classifier_test.py
+++ classifier_test.py
@@ -6,17 +6,18 @@ from keras.layers import Dense, Dropout, Activation
 from keras.layers import Embedding, Input
 from keras.layers import Conv1D, GlobalMaxPooling1D
 
-from sklearn.preprocessing import OneHotEncoder
-from keras.datasets import imdb
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
 
 import sys
 import json
 import re
+import pickle
 from tqdm import tqdm
 
-config = tf.ConfigProto()
+""" config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.3
-set_session(tf.Session(config=config))
+set_session(tf.Session(config=config)) """
 
 # set parameters:
 max_features = 5000
@@ -27,56 +28,54 @@ filters = 250
 kernel_size = 3
 epochs = 2
 
-print("Reading input file..")
+def transform_and_save(input_file):
+    print("Reading input file..")
 
-with open("./" + sys.argv[1]) as f:
-    data = json.load(f)
+    with open("./" + input_file) as f:
+        data = json.load(f)
 
-abstracts = []
-is_neuro = []
+    abstracts = []
+    is_neuro = []
 
-# Neuroscience MeSH-terms.
-reg = re.compile(r"(A08*|A11.650*|A08.637*|A11.671*|A08.675*|C10*|F01*|F02*|F03*|F04*)")
+    # Neuroscience MeSH-terms.
+    reg = re.compile(r"(A08*|A11.650*|A08.637*|A11.671*|A08.675*|C10*|F01*|F02*|F03*|F04*)")
 
-for article in tqdm(data, desc="Grabbing abstracts and mesh terms"):
-    abstract_parts = []
-    abstracts.append("\n".join([x["text"] for x in article["abstract"]]))
-    is_neuro.append(0)
-    for mesh_term in article["mesh_list"]:
-        if reg.match(mesh_term["mesh_id"]):
-            is_neuro[-1] = 1
-            break
+    for article in tqdm(data, desc="Grabbing abstracts and mesh terms"):
+        abstract_parts = []
+        abstracts.append("\n".join([x["text"] for x in article["abstract"]]))
+        is_neuro.append(False)
+        for mesh_term in article["mesh_list"]:
+            if reg.match(mesh_term["mesh_id"]):
+                is_neuro[-1] = True
+                break
 
-del data # Release the huge chunk of memory, ugly. TODO: structure code properly.
+    del data # Release the huge chunk of memory, ugly. TODO: structure code properly.
 
-print(abstracts[0])
-print(is_neuro[0])
+    print("\n",abstracts[0])
+    print(is_neuro[0])
 
-#input("check ram")
+    #input("check ram")
 
-print('Loading data...')
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
+    vectorizer = CountVectorizer(max_features=100000, binary=True, ngram_range=(1,1))
+    abstracts = vectorizer.fit_transform(abstracts)
+    print("abstract shape:", abstracts.shape)
 
-print (x_train.shape)
+    label_encoder = LabelEncoder()
+    is_neuro = label_encoder.fit_transform(is_neuro)
+    print("is_neuro shape", is_neuro.shape)
 
-#Since we are using output the size of 2, we will have to do one-hot encoding
-#x_test = to_categorical(x_test)
-#y_test = to_categorical(y_test)
+    one_hot_encoder = OneHotEncoder(sparse=False)
+    is_neuro = one_hot_encoder.fit_transform(is_neuro.reshape(-1,1))
+    print("is_neuro 1hot", is_neuro.shape)
 
+    with open("../abstracts_dump", "w") as f:
+        pickle.dump(abstracts, f)
+    
+    with open("../is_neuro_dump", "w") as f:
+        pickle.dump(is_neuro, f)
 
-print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-
-onehot_encoder = OneHotEncoder(sparse=False)
-y_test = onehot_encoder.fit_transform(y_test.reshape(-1, 1))
-y_train = onehot_encoder.transform(y_train.reshape(-1, 1))
-
-
+transform_and_save(sys.argv[1])
+sys.exit()
 
 print('Build model...')
 
