1a4a43152cd530081e50fd60f65ef33190af3b67
Antti Virtanen <haama92@gmail.com>
2019-01-25 20:17:22 +0200

    Implemented OneVsRest SVC as a baseline.

--- multilabel/sklearn_base.py
+++ multilabel/sklearn_base.py
@@ -1,9 +1,11 @@
 import sys, pickle
 
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import precision_score, recall_score, f1_score, hamming_loss
 
 def load_data(file_name):
     
@@ -14,26 +16,26 @@ print("Reading input files..")
 x = load_data("./" + sys.argv[1])
 y = load_data("./" + sys.argv[2])
 
-#print("Splitting..")
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-#del x,y # Free up ~3 gigabytes (with full dataset).
-
-print("Running..")
-
-classifier = MLPClassifier()
-mlb = MultiLabelBinarizer()
-vectorizer = TfidfVectorizer(min_df=3)
-
 print("Running vectorizer..")
+vectorizer = TfidfVectorizer(min_df=3)
 x = vectorizer.fit_transform(x)
+
 print("Binarizing labels..")
+mlb = MultiLabelBinarizer(sparse_output=True)
 y = mlb.fit_transform(y)
+
+print("Splitting..")
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+del x,y # Free up ~3 gigabytes (with full dataset).
+
+classifier = OneVsRestClassifier(LinearSVC(), n_jobs=20)
 
 print("Classifying..")
 
-classifier.fit(x, y)
+classifier.fit(x_train, y_train)
 
-#y_pred = classifier.predict(x_test)
-#print("Precision:", precision_score(y_test, y_pred))
-#print("Recall:", recall_score(y_test, y_pred))
-#print("F1:", f1_score(y_test, y_pred))
+y_pred = classifier.predict(x_test)
+
+print("Precision:", precision_score(y_test, y_pred, average='micro'))
+print("Recall:", recall_score(y_test, y_pred, average='micro'))
+print("F1:", f1_score(y_test, y_pred, average='micro'))
