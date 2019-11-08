6031bc07542a2f37645711df71e9bfa685b432c9
Antti Virtanen <haama92@gmail.com>
2019-11-08 18:14:45 +0200

    Cleanup.

--- multilabel/bert_fine_tune_multigpu.py
+++ multilabel/bert_fine_tune_multigpu.py
@@ -1,32 +1,30 @@
 import pickle, sys, lzma
 import tensorflow as tf
-import numpy as np
 import keras.backend as K
-import horovod.keras as hvd
-import math
 
 from scipy.sparse import lil_matrix
 
 from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
-from keras.layers import Dense, Flatten, Lambda, Dropout, Multiply, Permute, RepeatVector
+from keras.layers import Dense, Flatten, Lambda, Dropout
 from keras.models import Model
+from keras.utils import multi_gpu_model
 
 from keras_bert.loader import load_trained_model_from_checkpoint
 from keras_bert import AdamWarmup, calc_train_steps
 
 from sklearn.metrics import precision_recall_fscore_support
 
-hvd.init()
-
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
-config.gpu_options.visible_device_list = str(hvd.local_rank())
+config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
 K.set_session(tf.Session(config=config))
 
 # set parameters:
-batch_size = 1
+batch_size = 5
+gpus = 1
 learning_rate = 4e-5
-epochs = int(math.ceil( 20 // hvd.size() ))
+epochs = 15
+maxlen = 512
 
 class Metrics(Callback):
 
@@ -57,75 +55,49 @@ def build_model(abstracts_train, abstracts_test, labels_train, labels_test, sequ
                                                 training=False, trainable=True,
                                                 seq_len=sequence_len)
 
-    #slice_layer = Lambda(lambda x: tf.slice(x, [0, 0, 0], [-1, 1, -1]))(biobert.layers[-1].output)
+    slice_layer = Lambda(lambda x: K.slice(x, [0, 0, 0], [-1, 1, -1]))(biobert.layers[-1].output)
 
-    #flatten_layer = Flatten()(slice_layer)
+    flatten_layer = Flatten()(slice_layer)
 
-    #dropout_layer = Dropout(0.1)(flatten_layer)
+    dropout_layer = Dropout(0.1)(flatten_layer)
 
-    attention = Dense(1, activation="softmax", name="attention")(biobert.layers[-1].output)
-
-    # Hackish way to drop masking, flatten won't work otherwise.
-    attention = Lambda(lambda x: x, name="drop_mask_1")(attention)
-
-    attention = Flatten()(attention)
-
-    attention = RepeatVector(768)(attention)
-    
-    attention = Permute((2,1))(attention)
-
-    attention = Multiply()([attention, biobert.layers[-1].output])
-
-    # Hackish way to drop masking, flatten won't work otherwise.
-    attention = Lambda(lambda x: x, name="drop_mask_2")(attention)
-
-    attention = Flatten()(attention)
-    
-    output_layer = Dense(labels_train.shape[1], activation="sigmoid", name="decision")(attention)
+    output_layer = Dense(labels_train.shape[1], activation='sigmoid')(dropout_layer)
 
     model = Model(biobert.input, output_layer)
 
+    if gpus > 1:
+        model = multi_gpu_model(model, gpus=gpus)
+
     metrics = Metrics()
-    early_stopping = EarlyStopping(patience=10, verbose=1)
+    # --patience arg parser
+    early_stopping = EarlyStopping(patience=15, verbose=1)
 
     total_steps, warmup_steps = calc_train_steps(num_example=abstracts_train.shape[0],
                                                 batch_size=batch_size, epochs=epochs,
                                                 warmup_proportion=0.1)
 
     optimizer = AdamWarmup(total_steps, warmup_steps, lr=learning_rate)
-    optimizer = hvd.DistributedOptimizer(optimizer)
-
-    print("Compiling model")
 
     model.compile(loss="binary_crossentropy", optimizer=optimizer)
-
-    callbacks = [hvd.callbacks.BroadcastGlobalVariablesCallback(0), metrics, early_stopping]
     
-    if hvd.rank() == 0:
-        callbacks.append(ModelCheckpoint(sys.argv[5][:-3] + "-checkpoint-{epoch}.h5"))
-        print(model.summary(line_length=118))
-        print("Number of GPUs in use:", hvd.size())
-        print("Batch size:", batch_size)
-        print("Learning rate:", learning_rate)
+    print(model.summary(line_length=118))
+    print("Number of GPUs in use:", gpus)
+    print("Batch size:", batch_size)
+    print("Learning rate:", learning_rate)
 
     model.fit([abstracts_train, lil_matrix(abstracts_train.shape)], labels_train,
-        batch_size=batch_size, epochs=epochs, callbacks=callbacks,
+        batch_size=batch_size, epochs=epochs, callbacks=[metrics, ModelCheckpoint(sys.argv[5][:-3] + "-checkpoint-{epoch}.h5")],
         validation_data=[[abstracts_test, lil_matrix(abstracts_test.shape)], labels_test])
-    if hvd.rank() == 0:
-        model.save(sys.argv[5])
+    model.save(sys.argv[5])
 
 if __name__ == "__main__":
 
     print("Reading input files..")
 
-    abstracts_train = load_data(sys.argv[1])
-    print(abstracts_train.shape)
-    abstracts_train = abstracts_train[:,0:16]
-    print(abstracts_train.shape)
-    abstracts_test = load_data(sys.argv[2])
-    abstracts_test = abstracts_test[:,0:16]
-    labels_train = load_data(sys.argv[3])
-    labels_test = load_data(sys.argv[4])
+    abstracts_train = load_data(sys.argv[1])[0:1000]
+    abstracts_test = load_data(sys.argv[2])[0:100]
+    labels_train = load_data(sys.argv[3])[0:1000]
+    labels_test = load_data(sys.argv[4])[0:100]
 
     _, sequence_len = abstracts_train.shape
 
