b731b46f06d2d106a2a1420ba62eaba2db011b85
Antti Virtanen <haama92@gmail.com>
2019-11-13 15:33:43 +0200

    Adapted to use generators and work on any data.

--- multilabel/bert_fine_tune_multigpu.py
+++ multilabel/bert_fine_tune_multigpu.py
@@ -1,6 +1,13 @@
-import pickle, sys, lzma
+import pickle, sys, json, csv, gzip
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
 import keras.backend as K
+
+import numpy as np
+
+from math import ceil
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
@@ -16,108 +23,144 @@ from keras_bert import AdamWarmup, calc_train_steps
 
 from sklearn.metrics import precision_recall_fscore_support
 
-# set parameters:
-batch_size = 5
-gpus = 1
-learning_rate = 4e-5
-epochs = 15
-maxlen = 512
-
 def argparser():
     arg_parse = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    arg_parse.add_argument("--input_file", help="TSV input file.", metavar="FILE", required=True)
+    arg_parse.add_argument("--train", help="Processed train file.", metavar="FILE", required=True)
+    arg_parse.add_argument("--dev", help="Processed dev file.", metavar="FILE", required=True)
+    #arg_parse.add_argument("--test", help="Processed test file.", metavar="FILE", required=True)
     arg_parse.add_argument("--init_checkpoint", help="BERT tensorflow model with path ending in .ckpt", metavar="PATH", required=True)
-    arg_parse.add_argument("--output_model", help="Path to which save the finetuned model.", metavar="PATH", required=True)
+    arg_parse.add_argument("--bert_config", help="BERT config file.", metavar="FILE", required=True)
     arg_parse.add_argument("--vocab", help="Vocabulary to use.", metavar="FILE", required=True)
     arg_parse.add_argument("--batch_size", help="Batch size to use for finetuning.", metavar="INT", type=int, required=True)
     arg_parse.add_argument("--lr", "--learning_rate", help="Peak learning rate.", metavar="FLOAT", type=float, required=True)
     arg_parse.add_argument("--epochs", help="Max amount of epochs to run.", metavar="INT", type=int, required=True)
-    arg_parse.add_argument("--seq_len", help="BERT's maximum sequence length", metavar="INT", default=512, type=int)
-    arg_parse.add_argument("--gpus", help="Number of GPUs to use,", metavar="INT", default=1, type=int)
-    arg_parse.add_argument("--patience", help="Patience of early stopping. Early stopping disabled if 0.", metavar="INT", default=0, type=int)
+    arg_parse.add_argument("--output_file", help="Path to which save the finetuned model. Checkpoints will have the format `checkpoint-<epoch>-<output_file>`.", metavar="PATH", default="model.h5")
+    arg_parse.add_argument("--seq_len", help="BERT's maximum sequence length.", metavar="INT", default=512, type=int)
+    arg_parse.add_argument("--do_lower_case", help="Lowercase input text.", metavar="BOOL", default=False, type=bool)
+    arg_parse.add_argument("--gpus", help="Number of GPUs to use.", metavar="INT", default=1, type=int)
+    arg_parse.add_argument("--patience", help="Patience of early stopping. Early stopping disabled if -1.", metavar="INT", default=-1, type=int)
     arg_parse.add_argument("--threshold", help="Positive label prediction threshold.", metavar="FLOAT", default=0.5, type=float)
-    return arg_parse
+    arg_parse.add_argument("--eval_batch_size", help="Batch size for eval calls. Default value is the Keras default.", metavar="INT", default=32, type=int)
+    return arg_parse.parse_args()
+
+# Read example count from the first row a preprocessed file.
+def get_example_count(file_path):
+    with gzip.open(file_path, "rt") as f:
+        cr = csv.reader(f, delimiter="\t")
+        return int(next(cr)[0])
+
+# Read the first example from a file and return the length of the label list.
+def get_label_dim(file_path):
+    with gzip.open(file_path, "rt") as f:
+        cr = csv.reader(f, delimiter="\t")
+        next(cr) # Skip example number row.
+        return len(json.loads(next(cr)[1]))
+
+def data_generator(file_path, batch_size):
+    while True:
+        with gzip.open(file_path, "rt") as f:
+            cr = csv.reader(f, delimiter="\t")
+            next(cr) # Skip example number row.
+            text = []
+            labels = []
+            for line in cr:
+                if len(text) == batch_size:
+                    # Fun fact: the 2 inputs must be in a list, *not* a tuple. Why.
+                    yield ([np.asarray(text), np.zeros_like(text)], np.asarray(labels))
+                    text = []
+                    labels = []
+                text.append(np.asarray(json.loads(line[0])))
+                labels.append(np.asarray(json.loads(line[1])))
+            # Yield what is left as the last batch when file has been read to its end.
+            print("File done.")
+            yield ([np.asarray(text), np.zeros_like(text)], np.asarray(labels))
 
 class Metrics(Callback):
 
-    def on_epoch_end(self, batch, logs={}):
-        print("Predicting probabilities..")
-        labels_prob = self.model.predict([abstracts_test, lil_matrix(abstracts_test.shape)])
+    def __init__(self):
+        print("Metrics init, reading dev labels..")
+        self.labels_dev = []
+        with gzip.open(args.dev, "rt") as f:
+            cr = csv.reader(f, delimiter="\t")
+            next(cr) # Skip example number row.
+            for line in cr:
+                self.labels_dev.append(json.loads(line[1]))
+            self.labels_dev = lil_matrix(self.labels_dev)
+            print("Dev labels shape:", self.labels_dev.shape)
 
+    def on_epoch_end(self, epoch, logs=None):
+        print("Predicting probabilities..")
+        labels_prob = self.model.predict_generator(data_generator(args.dev, args.eval_batch_size), use_multiprocessing=True,
+                                                    steps=ceil( get_example_count(args.dev) / args.eval_batch_size))
         print("Probabilities to labels..")
         labels_pred = lil_matrix(labels_prob.shape, dtype='b')
-        labels_pred[labels_prob>0.5] = 1
-
-        precision, recall, f1, _ = precision_recall_fscore_support(labels_test, labels_pred, average="micro")
+        labels_pred[labels_prob>args.threshold] = 1
+        precision, recall, f1, _ = precision_recall_fscore_support(self.labels_dev, labels_pred, average="micro")
         print("Precision:", precision)
         print("Recall:", recall)
         print("F1-score:", f1, "\n")
 
-def load_data(file_name):
 
-    with lzma.open(file_name, "rb") as f:
-        return pickle.load(f)
-
-def build_model(abstracts_train, abstracts_test, labels_train, labels_test, sequence_len):
+def build_model(args):
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     #config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
     K.set_session(tf.Session(config=config))
 
-    checkpoint_file = "../../biobert_pubmed/biobert_model.ckpt"
-    config_file = "../../biobert_pubmed/bert_config.json"
-
-    biobert = load_trained_model_from_checkpoint(config_file, checkpoint_file,
+    print("Building model..")
+    bert = load_trained_model_from_checkpoint(args.bert_config, args.init_checkpoint,
                                                 training=False, trainable=True,
-                                                seq_len=sequence_len)
+                                                seq_len=args.seq_len)
 
-    slice_layer = Lambda(lambda x: K.slice(x, [0, 0, 0], [-1, 1, -1]))(biobert.layers[-1].output)
+    slice_layer = Lambda(lambda x: K.slice(x, [0, 0, 0], [-1, 1, -1]))(bert.layers[-1].output)
 
     flatten_layer = Flatten()(slice_layer)
 
     dropout_layer = Dropout(0.1)(flatten_layer)
 
-    output_layer = Dense(labels_train.shape[1], activation='sigmoid')(dropout_layer)
+    output_layer = Dense(get_label_dim(args.train), activation='sigmoid')(dropout_layer)
 
-    model = Model(biobert.input, output_layer)
+    model = Model(bert.input, output_layer)
 
-    if gpus > 1:
-        model = multi_gpu_model(model, gpus=gpus)
+    if args.gpus > 1:
+        template_model = model
+        model = multi_gpu_model(template_model, gpus=args.gpus)
 
-    metrics = Metrics()
-    # --patience arg parser
-    early_stopping = EarlyStopping(patience=15, verbose=1)
+    callbacks = [Metrics()]
 
-    total_steps, warmup_steps = calc_train_steps(num_example=abstracts_train.shape[0],
-                                                batch_size=batch_size, epochs=epochs,
+    if args.patience > -1:
+        callbacks.append(EarlyStopping(patience=args.patience, verbose=1))
+
+    callbacks.append(ModelCheckpoint("checkpoint-{epoch}-" + args.output_file))
+
+    total_steps, warmup_steps =  calc_train_steps(num_example=get_example_count(args.train),
+                                                batch_size=args.batch_size, epochs=args.epochs,
                                                 warmup_proportion=0.1)
 
-    optimizer = AdamWarmup(total_steps, warmup_steps, lr=learning_rate)
+    optimizer = AdamWarmup(total_steps, warmup_steps, lr=args.lr)
 
     model.compile(loss="binary_crossentropy", optimizer=optimizer)
     
     print(model.summary(line_length=118))
-    print("Number of GPUs in use:", gpus)
-    print("Batch size:", batch_size)
-    print("Learning rate:", learning_rate)
+    print("Number of GPUs in use:", args.gpus)
+    print("Batch size:", args.batch_size)
+    print("Learning rate:", args.lr)
 
-    model.fit([abstracts_train, lil_matrix(abstracts_train.shape)], labels_train,
-        batch_size=batch_size, epochs=epochs, callbacks=[metrics, ModelCheckpoint(sys.argv[5][:-3] + "-checkpoint-{epoch}.h5")],
-        validation_data=[[abstracts_test, lil_matrix(abstracts_test.shape)], labels_test])
-    model.save(sys.argv[5])
+
+    # ceil( get_example_count(args.train) / args.batch_size )
+    model.fit_generator(data_generator(args.train, args.batch_size),
+                        steps_per_epoch=ceil( get_example_count(args.train) / args.batch_size ), 
+                        use_multiprocessing=True, epochs=args.epochs, callbacks=callbacks,
+                        validation_data=data_generator(args.dev, args.eval_batch_size),
+                        validation_steps=ceil( get_example_count(args.dev) / args.eval_batch_size ))
+    
+    if args.gpus > 1:
+        template_model.save(args.output_file)
+    else:
+        model.save(args.output_file)
 
 if __name__ == "__main__":
 
-    args = argparser().parse_args()
-
-    print("Reading input files..")
-
-    # abstracts_train = load_data(sys.argv[1])[0:1000]
-    # abstracts_test = load_data(sys.argv[2])[0:100]
-    # labels_train = load_data(sys.argv[3])[0:1000]
-    # labels_test = load_data(sys.argv[4])[0:100]
-
-    _, sequence_len = abstracts_train.shape
-
-    build_model(abstracts_train, abstracts_test, labels_train, labels_test, sequence_len)
+    args = argparser()
+    build_model(args)
