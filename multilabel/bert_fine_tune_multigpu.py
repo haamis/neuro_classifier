cd243e73154d473302f319e4741898baa1caacef
Antti Virtanen <haama92@gmail.com>
2019-11-27 11:22:07 +0200

    Added options for top N labels, dropout, checkpointing.

--- multilabel/bert_fine_tune_multigpu.py
+++ multilabel/bert_fine_tune_multigpu.py
@@ -1,9 +1,11 @@
-import pickle, sys, json, csv, gzip
+import pickle, sys, csv, gzip, json
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.ERROR)
 import keras.backend as K
+
+import keras
 
 import numpy as np
 
@@ -14,7 +16,7 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from scipy.sparse import lil_matrix
 
 from keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
-from keras.layers import Dense, Flatten, Lambda, Dropout
+from keras.layers import Average, Maximum, Concatenate, Conv1D, Dense, Dropout, Flatten, GlobalAveragePooling1D, GlobalMaxPooling1D, Lambda, Reshape
 from keras.models import Model
 from keras.utils import multi_gpu_model
 
@@ -32,11 +34,15 @@ def argparser():
     arg_parse.add_argument("--batch_size", help="Batch size to use for finetuning.", metavar="INT", type=int, required=True)
     arg_parse.add_argument("--lr", "--learning_rate", help="Peak learning rate.", metavar="FLOAT", type=float, required=True)
     arg_parse.add_argument("--epochs", help="Max amount of epochs to run.", metavar="INT", type=int, required=True)
+    arg_parse.add_argument("--dev_all", help="Processed dev file with all labels. Use if only training on top N labels.", metavar="FILE", default=None)
+    arg_parse.add_argument("--label_mapping", help="Mapping from N labels to all labels. Use if only training on top N labels.", metavar="FILE", default=None)
     arg_parse.add_argument("--output_file", help="Path to which save the finetuned model. Checkpoints will have the format `<output_file>.checkpoint-<epoch>`.", metavar="PATH", default="model.h5")
     arg_parse.add_argument("--seq_len", help="BERT's maximum sequence length.", metavar="INT", default=512, type=int)
     arg_parse.add_argument("--do_lower_case", help="Lowercase input text.", metavar="BOOL", default=False, type=bool)
+    arg_parse.add_argument("--dropout", help="Dropout rate between BERT and the decision layer.", metavar="FLOAT", default=0.1, type=float)
     arg_parse.add_argument("--gpus", help="Number of GPUs to use.", metavar="INT", default=1, type=int)
     arg_parse.add_argument("--patience", help="Patience of early stopping. Early stopping disabled if -1.", metavar="INT", default=-1, type=int)
+    arg_parse.add_argument("--checkpoint_interval", help="Interval between checkpoints. 1 for every epoch, 0 to disable.", metavar="INT", default=0, type=int)
     arg_parse.add_argument("--threshold_start", help="Positive label prediction threshold range start.", metavar="FLOAT", default=0.1, type=float)
     arg_parse.add_argument("--threshold_end", help="Positive label prediction threshold range end, exclusive.", metavar="FLOAT", default=1.0, type=float)
     arg_parse.add_argument("--threshold_step", help="Positive label prediction threshold range step.", metavar="FLOAT", default=0.1, type=float)
@@ -78,29 +84,46 @@ class Metrics(Callback):
 
     def __init__(self):
         print("Metrics init, reading dev labels..")
-        self.labels_dev = []
-        with gzip.open(args.dev, "rt") as f:
+        self.all_labels = []
+        if args.label_mapping is not None:
+            file_name = args.dev_all
+        else:
+            file_name = args.dev
+        with gzip.open(file_name, "rt") as f:
             cr = csv.reader(f, delimiter="\t")
             next(cr) # Skip example number row.
             for line in cr:
-                self.labels_dev.append(json.loads(line[1]))
-            self.labels_dev = lil_matrix(self.labels_dev)
-            print("Dev labels shape:", self.labels_dev.shape)
-
+                self.all_labels.append(json.loads(line[1]))
+            self.all_labels = lil_matrix(self.all_labels)
+            print("Dev labels shape:", self.all_labels.shape)
+        if args.dev_all is not None:
+            with open(args.label_mapping) as f:
+                self.labels_mapping = json.loads(f.read())
+        
     def on_epoch_end(self, epoch, logs=None):
         print("Predicting probabilities..")
         labels_prob = self.model.predict_generator(data_generator(args.dev, args.eval_batch_size), use_multiprocessing=True,
-                                                    steps=ceil( get_example_count(args.dev) / args.eval_batch_size))
+                                                    steps=ceil(get_example_count(args.dev) / args.eval_batch_size))
         print("Probabilities to labels..")
+        if args.label_mapping is not None:
+            full_labels_prob = np.zeros(self.all_labels.shape)
+            for i, probs in enumerate(labels_prob):
+               np.put(full_labels_prob[i], self.labels_mapping, probs)
+
+            labels_prob = full_labels_prob
+
         for threshold in np.arange(args.threshold_start, args.threshold_end, args.threshold_step):
-            labels_pred = lil_matrix(labels_prob.shape, dtype='b')
             print("Threshold:", threshold)
+            labels_pred = lil_matrix(labels_prob.shape, dtype='b')
             labels_pred[labels_prob>threshold] = 1
-            precision, recall, f1, _ = precision_recall_fscore_support(self.labels_dev, labels_pred, average="micro")
+            precision, recall, f1, _ = precision_recall_fscore_support(self.all_labels, labels_pred, average="micro")
             print("Precision:", precision)
             print("Recall:", recall)
             print("F1-score:", f1, "\n")
 
+
+def loss_fn(y_true, y_pred):
+    return keras.losses.binary_crossentropy(y_true, y_pred) + keras.losses.categorical_hinge(y_true, y_pred)
 
 def build_model(args):
 
@@ -114,11 +137,38 @@ def build_model(args):
                                                 training=False, trainable=True,
                                                 seq_len=args.seq_len)
 
-    slice_layer = Lambda(lambda x: K.slice(x, [0, 0, 0], [-1, 1, -1]))(bert.layers[-1].output)
+    slice_layer = Lambda(lambda x: K.slice(x, [0, 0, 0], [-1, 1, -1]))(bert.get_layer("Encoder-12-FeedForward-Norm").output)
+
+    # slices = []
+    # for i in range(1, 13):
+    #     layer_name = "Encoder-" + str(i) + "-FeedForward-Norm"
+    #     cls_slice = Lambda(lambda x: K.slice(x, [0, 0, 0], [-1, 1, -1]))(bert.get_layer(layer_name).output)
+    #     # Simple layer to drop masking as it does not pass it along with layer.compute_mask().
+    #     slices.append(Lambda(lambda x: x)(cls_slice))
+
+    # cls_average = Average()(slices)
+    # # This reshape might seem odd, but is required.
+    # slices_shaped = Reshape((768,))(cls_average)
+    # print(slices_shaped.shape)
+
+    # Simple layer to drop masking as it does not pass it along with layer.compute_mask().
+    # drop_mask = Lambda(lambda x: x)(slice_layer)
+
+    # conv1 = Conv1D(768, 25, padding='valid', activation='relu', strides=1)(drop_mask)
+    # conv2 = Conv1D(768, 10, padding='valid', activation='relu', strides=1)(drop_mask)
+    # conv3 = Conv1D(768, 3, padding='valid', activation='relu', strides=1)(drop_mask)
+    # conv4 = Conv1D(768, 1, padding='valid', activation='relu', strides=1)(drop_mask)
+
+    # max_pool1 = GlobalMaxPooling1D()(conv1)
+    # max_pool2 = GlobalMaxPooling1D()(conv2)
+    # max_pool3 = GlobalMaxPooling1D()(conv3)
+    # max_pool4 = GlobalMaxPooling1D()(conv4)
+
+    # concat = Concatenate()([max_pool1, max_pool2, max_pool3, max_pool4])
 
     flatten_layer = Flatten()(slice_layer)
 
-    dropout_layer = Dropout(0.1)(flatten_layer)
+    dropout_layer = Dropout(args.dropout)(flatten_layer)
 
     output_layer = Dense(get_label_dim(args.train), activation='sigmoid')(dropout_layer)
 
@@ -133,7 +183,8 @@ def build_model(args):
     if args.patience > -1:
         callbacks.append(EarlyStopping(patience=args.patience, verbose=1))
 
-    callbacks.append(ModelCheckpoint(args.output_file + ".checkpoint-{epoch}"))
+    if args.checkpoint_interval > 0:
+        callbacks.append(ModelCheckpoint(args.output_file + ".checkpoint-{epoch}",  period=args.checkpoint_interval))
 
     total_steps, warmup_steps =  calc_train_steps(num_example=get_example_count(args.train),
                                                 batch_size=args.batch_size, epochs=args.epochs,
@@ -142,20 +193,19 @@ def build_model(args):
     optimizer = AdamWarmup(total_steps, warmup_steps, lr=args.lr)
 
     model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
-    
+
     print(model.summary(line_length=118))
     print("Number of GPUs in use:", args.gpus)
     print("Batch size:", args.batch_size)
     print("Learning rate:", args.lr)
 
 
-    # ceil( get_example_count(args.train) / args.batch_size )
     model.fit_generator(data_generator(args.train, args.batch_size),
-                        steps_per_epoch=ceil( get_example_count(args.train) / args.batch_size ), 
+                        steps_per_epoch=ceil( get_example_count(args.train) / args.batch_size ),
                         use_multiprocessing=True, epochs=args.epochs, callbacks=callbacks,
                         validation_data=data_generator(args.dev, args.eval_batch_size),
                         validation_steps=ceil( get_example_count(args.dev) / args.eval_batch_size ))
-    
+
     if args.gpus > 1:
         template_model.save(args.output_file)
     else:
